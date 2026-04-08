9a853809d3d8d8109c2383939bdfe4a0871663a5
Kristijonas <krnorkus@gmail.com>
2026-04-08 17:57:18 +0300

    testing solution for Exercise 9

--- fs-pokedex-main/tests/e2etests.spec.js
+++ fs-pokedex-main/tests/e2etests.spec.js
@@ -10,4 +10,21 @@ describe("Pokedex", () => {
       ),
     ).toBeVisible();
   });
+
+  describe("Navigated to front page", () => {
+    beforeEach(async ({ page }) => {
+      await page.goto("/");
+      await expect(page.getByText("ivysaur")).toBeVisible();
+      await expect(
+        page.getByText(
+          "Pokémon and Pokémon character names are trademarks of Nintendo.",
+        ),
+      ).toBeVisible();
+    });
+    test("can open a particular pokemons page", async ({ page }) => {
+      const locator = page.getByRole("link", { name: "weedle" });
+      await locator.click();
+      await expect(page.getByText("run away")).toBeVisible();
+    });
+  });
 });
