993a4fa85558eebf47d12d9b209d0e24025afbe2
Kristijonas <krnorkus@gmail.com>
2026-04-09 15:51:47 +0300

    another not so big change

--- fs-pokedex-main/app.js
+++ fs-pokedex-main/app.js
@@ -12,7 +12,7 @@ const start = async () => {
 }
 
 app.get('/version', (req, res) => {
-  res.send('1.0.1') // change this string to ensure a new version deployed
+  res.send('1.0.2') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
